8df32bc1cccef5a739c242dbd2f391f8651a81de
Aashish Vinu <shijidhanvanthvp@gmail.com>
2023-06-01 13:58:55 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int a,b,c,d,e,f,g,x;
+    printf("Enter 7 numbers: ");
+    scanf("%d%d%d%d%d%d%d",&a, &b, &c, &d, &e, &f, &g);
+    x = ((a - b / c * d + e) * (f + g))
+    printf("Solution is %d\n", x);
+}
