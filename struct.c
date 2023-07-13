730d3eed528eff1b97a00c53d01fb7b65e0f915b
Aashish Vinu <mail@aashishvinu.me>
2023-07-13 13:31:06 +0530

    Create struct.c

---
+++ struct.c
@@ -0,0 +1,17 @@
+#include<stdio.h> struct emplovee
+intid,salary;
+char name [25];
+}emp[100];
+void main)
+inti,n;
+printf("Enter the no of employees\n");
+scanf("%d",&n);
+printf("Enter employee info as id, name , salaryIn");
+for(i=0;i<n;it+)
+scanf("%d %s
+%d",&empli].id,emp[i].name,&emp[1].salary);
+printf("InEMP_NAME\tEMP_NAME\tEMP_SALIn");
+or(i=0;i<n;it+)
+{
+printf(*%dltlt%s\th%dIn",emp[i].id,emp[i].name,emp ['].salary);
+  }}
